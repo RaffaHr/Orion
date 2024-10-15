a0271b20056231f5a1c9f3d0639af6fe47a58cc7
RaffaHr <128329385+RaffaHr@users.noreply.github.com>
2024-10-15 20:51:56 +0000

    update structure files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // Certifique-se de que o diretório de saída esteja correto
+    outDir: 'dist'
   },
   server: {
-    port: 3000, // Você pode escolher a porta que preferir
-  },
+    port: 3000 // ou qualquer porta que você prefira
+  }
 });

97b2bea12ca19d8a580f7ef1514bbc476cddeb18
RaffaHr <128329385+RaffaHr@users.noreply.github.com>
2024-10-15 18:38:39 +0000

    update structure files

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,19 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// Importando o caminho do módulo 'path'
+import path from 'path';
+
+// Definindo a configuração do Vite
 export default defineConfig({
+  // Especificando o diretório de entrada
+  root: path.resolve(__dirname, 'frontend'), // Caminho para a pasta 'frontend'
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+  // Configuração para o diretório de saída
+  build: {
+    outDir: path.resolve(__dirname, 'frontend', 'dist'), // Caminho para a pasta 'dist' dentro de 'frontend'
   },
-})
+  server: {
+    port: 3000, // Porta do servidor de desenvolvimento
+  },
+});
